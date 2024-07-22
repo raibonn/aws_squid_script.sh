2e64019e72ae64792c9c2df83f5600bee8ba38a5
raibonn <66820348+raibonn@users.noreply.github.com>
2024-07-23 03:39:41 +0900

    Update lustdevsproxy.sh
    いろいろ修正した
    コメントアウトは使うか微妙な部分
    cacheやcoredumpは/dev/nullに送っていいと思う

--- lustdevsproxy.sh
+++ lustdevsproxy.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 sudo su
+
 apt update -y
 apt upgrade -y
+
 apt install squid openssl
+
 systemctl enable squid
+
+#/etc/squid/squid.conf
 :>| /etc/squid/squid.conf
+
 echo 'include /etc/squid/conf.d/*.conf' | sudo tee -a tee /etc/squid/squid.conf > /dev/null
 echo 'http_access allow all' | sudo tee -a /etc/squid/squid.conf > /dev/null
 echo 'http_port 3128' | sudo tee -a /etc/squid/squid.conf > /dev/null
@@ -30,8 +36,18 @@ echo 'cache deny NOCACHE' | sudo tee -a /etc/squid/squid.conf > /dev/null
 echo 'access_log none' | sudo tee -a /etc/squid/squid.conf > /dev/null
 echo 'cache_store_log none' | sudo tee -a /etc/squid/squid.conf > /dev/null
 echo 'cache_log /dev/null' | sudo tee -a /etc/squid/squid.conf > /dev/null
-#echo 'cache_dir none /dev/null' | sudo tee -a /etc/squid/squid.conf > /dev/null
+echo 'cache_dir none /dev/null' | sudo tee -a /etc/squid/squid.conf > /dev/null
+echo 'max_filedescriptors 8196' | sudo tee -a /etc/squid/squid.conf > /dev/null
+
 #echo '* - nofile 8196' | sudo tee -a /etc/security/limits.conf > /dev/null
+
 #echo 'max_filedescriptors 16384' | sudo tee -a /etc/security/limits.conf
+
 #echo "max_filedescriptors 32768" >> /etc/squid/local_bottom.conf
+
 systemctl start squid
+
+#/etc/systemd/system/multi-user.target.wants/squid.service
+sed -i '/NotifyAccess=all/i LimitNOFILE=8196' /etc/systemd/system/multi-user.target.wants/squid.service
+sudo systemctl daemon-reload
+systemctl restart squid
