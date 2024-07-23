c7e65b4a360748a28b873d280d1f0dd5e764e283
raibonn <66820348+raibonn@users.noreply.github.com>
2024-07-23 14:18:47 +0900

    Update lustdevsproxy.sh
    Socks5を実装

--- lustdevsproxy.sh
+++ lustdevsproxy.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
+
+#excute as root
 sudo su
 
+#Refresh System
 apt update -y
 apt upgrade -y
 
+###HTTP###
+#install HTTP Proxy
 apt install squid openssl
 
+#Make symlink
 systemctl enable squid
 
+#Clear Config
 #/etc/squid/squid.conf
 :>| /etc/squid/squid.conf
 
+#Write Config
 echo 'include /etc/squid/conf.d/*.conf' | sudo tee -a tee /etc/squid/squid.conf > /dev/null
 echo 'http_access allow all' | sudo tee -a /etc/squid/squid.conf > /dev/null
 echo 'http_port 3128' | sudo tee -a /etc/squid/squid.conf > /dev/null
@@ -46,10 +54,34 @@ echo 'max_filedescriptors 8196' | sudo tee -a /etc/squid/squid.conf > /dev/null
 
 #echo "max_filedescriptors 32768" >> /etc/squid/local_bottom.conf
 
+#Start HTTP Proxy port:3128
 systemctl start squid
 
+#Edit limit
 #/etc/systemd/system/multi-user.target.wants/squid.service
 #sed -i '/NotifyAccess=all/i LimitNOFILE=8196' /etc/systemd/system/multi-user.target.wants/squid.service
 sed -i '/NotifyAccess=all/i LimitNOFILE=8196' /usr/lib/systemd/system/squid.service
 sudo systemctl daemon-reload
 systemctl restart squid
+
+###Socks5###
+#install SOCKS5 Proxy
+apt install dante-server
+
+#Config Edit
+echo 'internal: 0.0.0.0 port = 1080' | sudo tee -a /etc/danted.conf > /dev/null
+echo 'external: enX0' | sudo tee -a /etc/danted.conf > /dev/null
+echo 'socksmethod: none' | sudo tee -a /etc/danted.conf > /dev/null
+echo 'clientmethod: none' | sudo tee -a /etc/danted.conf > /dev/null
+echo '' | sudo tee -a /etc/danted.conf > /dev/null
+echo 'client pass {' | sudo tee -a /etc/danted.conf > /dev/null
+echo '  from: 0.0.0.0/0 to: 0.0.0.0/0' | sudo tee -a /etc/danted.conf > /dev/null
+echo '}' | sudo tee -a /etc/danted.conf > /dev/null
+echo '' | sudo tee -a /etc/danted.conf > /dev/null
+echo 'socks pass {' | sudo tee -a /etc/danted.conf > /dev/null
+echo '  from: 0.0.0.0/0 to: 0.0.0.0/0' | sudo tee -a /etc/danted.conf > /dev/null
+echo '}' | sudo tee -a /etc/danted.conf > /dev/null
+
+#Activate Proxy
+systemctl enable danted
+systemctl restart danted
