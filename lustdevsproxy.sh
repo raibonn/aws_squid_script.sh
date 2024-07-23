c5c31fcfb908d1873da6f43e0a0caea62e7b3944
raibonn <66820348+raibonn@users.noreply.github.com>
2024-07-23 20:02:46 +0900

    Update lustdevsproxy.sh

--- lustdevsproxy.sh
+++ lustdevsproxy.sh
@@ -16,7 +16,8 @@ sudo systemctl enable squid
 
 #Clear Config
 #/etc/squid/squid.conf
-echo -n >| sudo tee /etc/squid/squid.conf
+sudo rm /etc/squid/squid.conf
+sudo touch /etc/squid/squid.conf
 
 #Write Config
 echo 'include /etc/squid/conf.d/*.conf' | sudo tee -a tee /etc/squid/squid.conf > /dev/null
